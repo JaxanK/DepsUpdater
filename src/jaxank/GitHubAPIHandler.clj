22ef6cfe3e66669dc2e530a32e2c1191460ed1e3
xadvent <piercestrutt@gmail.com>
2024-03-05 18:21:29 -0700

    feat: working gh api call
    `fetch-latest-version` now returns proper version numbers.
    
    "GITHUB_TOKEN" needs to be set in Systen/env for it to function properly.
    This was the easiest way to manage authentication for the API.

--- src/jaxank/GitHubAPIHandler.clj
+++ src/jaxank/GitHubAPIHandler.clj
@@ -1,4 +1,7 @@
-(ns jaxank.GitHubAPIHandler)
+(ns jaxank.GitHubAPIHandler
+  (:require [clojure.string :as str]
+            [clj-http.client :as client]
+            [cheshire.core :as json]))
 
 (defn mock-fetch-latest-version [repo]
   (let [mock-responses {"jaxank/crazy-cool-plugin" {:mvn/version "v0.2.0"}
@@ -7,8 +10,21 @@
     (when response
       (:mvn/version response))))
 
-(defn fetch-latest-version [repo]
-  ;; Placeholder for the actual API call logic
-  ;; For now, it could return a mock response or simply nil
-  (mock-fetch-latest-version repo))
+(defn get-owner-repo
+  "Dependency: owner/name {:mvn/version 'version number'}"
+  [full-dependency]
+  (let [split-string (first (clojure.string/split full-dependency #" "))]
+    split-string))
 
+(defn fetch-latest-version
+  "Fetches the latest version of a GitHub repository based on a dependency string."
+  [repo]
+  (let [url (str "https://api.github.com/repos/" (get-owner-repo repo) "/releases/latest")
+        response (client/get url {:headers {"Authorization" (str "Bearer " (System/getenv "GITHUB_TOKEN"))}
+                                  :as :json})]
+    (if (= 200 (:status response))
+      (-> response :body :tag_name)
+      (println "Failed to fetch latest version for" repo "with status code" (:status response)))))
+
+; NOTE: This requires a system env variable named "GITHUB_TOKEN"
+(fetch-latest-version "neovim/neovim {:mvn/version '0.0.1'}") 
